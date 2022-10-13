41a69de2ff7fb1ada9cf55c13e578b4e0ad4cf8c
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-10-13 16:25:27 -0600

    Write code to set state to movie data and render multiple movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,21 @@
 import './App.css';
+import React, { Component } from 'react'
+import movieData from '../../movieData.js'
+import Movies from '../Movies/Movies.js'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = movieData
+  }
+  render() {
+    return (
+      <main className='App'>
+        <h1>Rancid Tomatillos</h1>
+        <Movies movies={this.state.movies} />
+      </main>
+    )
+  }
+}
 
 export default App;
