918f86bc1cd972ec3d3b7bea7f966fdf0c8b8727
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-10-13 16:27:51 -0600

    write function component to display movie cards to page

---
+++ src/components/MovieCards/MovieCards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MovieCards.css'
+
+const MovieCard = ({id, posterPath, title, rating}) => {
+  return (
+    <div className='movie-cards'>
+      <img src={posterPath}/>
+      <h3>{title}</h3>
+      <p>{rating}</p>
+    </div>
+  )
+}
+
+export default MovieCard;
