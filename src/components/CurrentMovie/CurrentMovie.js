e486196b71cad982e226a6bcae53874d2692dc01
Leah Young <leah.young21@alumni.colostate.edu>
2022-10-14 16:28:18 -0600

    add styling to CurrentMovie page

--- src/components/CurrentMovie/CurrentMovie.js
+++ src/components/CurrentMovie/CurrentMovie.js
@@ -5,17 +5,20 @@ const CurrentMovie = ({ movies }) => {
     console.log(movies)
     return (
         <div className="single-movie-page">
+            <img className="backdrop-image" src={movies.movie.backdrop_path} alt={`${movies.movie.title} poster`} />
+            <div className="movie-info-container">
             <div className="movie-info">
                 <h3>{movies.movie.overview}</h3>
-                <h3>{movies.movie.release_date}</h3>
-                <h3>{movies.movie.average_rating}</h3>
-                <h3>{movies.movie.budget}</h3>
-                <h3>{movies.movie.revenue}</h3>
-                <h3>{movies.movie.runtime}</h3>
-                <h3>{movies.movie.genres}</h3>
-                <h3>{movies.movie.tagline}</h3>
+                <h3>{`Release date: ${movies.movie.release_date}`}</h3>
+                <h3>{`Rating: ${movies.movie.average_rating.toFixed(1)}⭐`}</h3>
+                <h3>{`Budget: $${movies.movie.budget}`}</h3>
+                <h3>{`Revenue: $${movies.movie.revenue}`}</h3>
+                <h3>{`Runtime: ${movies.movie.runtime}`}</h3>
+                <h3>{`Genres: ${movies.movie.genres.join(", ")}`}</h3>
+                <h3>{`Tagline: '${movies.movie.tagline}'`}</h3>
             </div>
-            <img src={movies.movie.poster_path} alt={`${movies.movie.title} poster`} />
+            <img className="poster-image" src={movies.movie.poster_path} alt={`${movies.movie.title} poster`} />
+            </div> 
         </div>
     )
 }
